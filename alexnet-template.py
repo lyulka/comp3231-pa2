0539a62113fa2884dda5bd804bec8c31a3a5607c
Adrio Alimin <strelka@connect.hku.hk>
2021-12-27 18:02:32 +0800

    Done question 1.

---
+++ alexnet-template.py
@@ -0,0 +1,298 @@
+import os
+import threading
+import time
+from functools import wraps
+
+import torch
+import torch.nn as nn
+import torch.distributed.autograd as dist_autograd
+import torch.distributed.rpc as rpc
+import torch.multiprocessing as mp
+import torch.optim as optim
+from torch.distributed.optim import DistributedOptimizer
+from torch.distributed.rpc import RRef
+
+
+
+num_classes = 1000
+
+
+class Stage0(torch.nn.Module):
+    def __init__(self):
+        super(Stage0, self).__init__()
+        self._lock = threading.Lock()
+
+        self.layer2 = torch.nn.Conv2d(3, 64, kernel_size=(11, 11), stride=(4, 4), padding=(2, 2))
+        self.layer3 = torch.nn.ReLU(inplace=True)
+        self.layer4 = torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=0, dilation=1, ceil_mode=False)
+
+    def forward(self, x_rref):
+        x = x_rref.to_here().to("cpu")
+        with self._lock:
+            out2 = self.layer2(x)
+            out3 = self.layer3(out2)
+            out4 = self.layer4(out3)
+
+        print("end of stage 0 forward")
+        return out4
+
+    def parameter_rrefs(self):
+        r"""
+        Create one RRef for each parameter in the given local module, and return a
+        list of RRefs.
+        """
+        return [RRef(p) for p in self.parameters()]
+
+
+class Stage1(torch.nn.Module):
+    def __init__(self):
+        super(Stage1, self).__init__()
+        self._lock = threading.Lock()
+
+        self.layer1 = torch.nn.Conv2d(64, 192, kernel_size=(5, 5), stride=(1, 1), padding=(2, 2))
+        self.layer2 = torch.nn.ReLU(inplace=True)
+        self.layer3 = torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=0, dilation=1, ceil_mode=False)
+
+    
+
+    def forward(self, x_rref):
+        x = x_rref.to_here().to("cpu")
+        with self._lock:
+            out1 = self.layer1(x)
+            out2 = self.layer2(out1)
+            out3 = self.layer3(out2)
+        
+        print("end of stage 1 forward")
+        return out3
+
+    def parameter_rrefs(self):
+        r"""
+        Create one RRef for each parameter in the given local module, and return a
+        list of RRefs.
+        """
+        return [RRef(p) for p in self.parameters()]
+
+
+class Stage2(torch.nn.Module):
+    def __init__(self):
+        super(Stage2, self).__init__()
+        self._lock = threading.Lock()
+
+        self.layer1 = torch.nn.Conv2d(192, 384, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.layer2 = torch.nn.ReLU(inplace=True)
+        self.layer3 = torch.nn.Conv2d(384, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.layer4 = torch.nn.ReLU(inplace=True)
+        self.layer5 = torch.nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.layer6 = torch.nn.ReLU(inplace=True)
+
+
+    def forward(self, x_rref):
+        x = x_rref.to_here().to("cpu")
+        with self._lock:
+            out1 = self.layer1(x)
+            out2 = self.layer2(out1)
+            out3 = self.layer3(out2)
+            out4 = self.layer4(out3)
+            out5 = self.layer5(out4)
+            out6 = self.layer6(out5)
+
+        print("end of stage 2 forward")
+        return out6
+
+    def parameter_rrefs(self):
+        r"""
+        Create one RRef for each parameter in the given local module, and return a
+        list of RRefs.
+        """
+        return [RRef(p) for p in self.parameters()]
+
+class Stage3(torch.nn.Module):
+    def __init__(self):
+        super(Stage3, self).__init__()
+        self._lock = threading.Lock()
+
+        self.layer1 = torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=0, dilation=1, ceil_mode=False)
+        self.layer4 = torch.nn.Dropout(p=0.5)
+        self.layer5 = torch.nn.Linear(in_features=2304, out_features=4096, bias=True)
+        self.layer6 = torch.nn.ReLU(inplace=True)
+        self.layer7 = torch.nn.Dropout(p=0.5)
+        self.layer8 = torch.nn.Linear(in_features=4096, out_features=4096, bias=True)
+        self.layer9 = torch.nn.ReLU(inplace=True)
+        self.layer10 = torch.nn.Linear(in_features=4096, out_features=1000, bias=True)
+
+    
+
+    def forward(self, x_rref):
+        x = x_rref.to_here().to("cpu")
+        with self._lock:
+            out1 = self.layer1(x)
+            out2 = out1.size(0)
+            out3 = out1.view(out2, 2304)
+            out4 = self.layer4(out3)
+            out5 = self.layer5(out4)
+            out6 = self.layer6(out5)
+            out7 = self.layer7(out6)
+            out8 = self.layer8(out7)
+            out9 = self.layer9(out8)
+            out10 = self.layer10(out9)
+        
+        print("end of stage 3 forward")
+
+        return out10
+
+    def parameter_rrefs(self):
+        r"""
+        Create one RRef for each parameter in the given local module, and return a
+        list of RRefs.
+        """
+        return [RRef(p) for p in self.parameters()]
+
+
+class DistAlexNet(nn.Module):
+    """
+    Assemble two parts as an nn.Module and define pipelining logic
+    """
+    def __init__(self, split_size, workers, *args, **kwargs):
+        super(DistAlexNet, self).__init__()
+
+        self.split_size = split_size
+
+        # Put the first stage on workers[0]
+        self.p1_rref = rpc.remote(
+            workers[0],
+            Stage0,
+            args = args,
+            kwargs = kwargs,
+            timeout=0
+        )
+        # Put the second stage on workers[1]
+        self.p2_rref = rpc.remote(
+            workers[1],
+            Stage1,
+            args = args,
+            kwargs = kwargs,
+            timeout=0
+        )
+        # Put the third stage on workers[2]
+        self.p3_rref = rpc.remote(
+            workers[2],
+            Stage2,
+            args = args,
+            kwargs = kwargs,
+            timeout=0
+        )
+        # Put the fourth stage on workers[3]
+        self.p4_rref = rpc.remote(
+            workers[3],
+            Stage3,
+            args = args,
+            kwargs = kwargs,
+            timeout=0
+        )
+
+    def forward(self, xs):
+        # Split the input batch xs into micro-batches, and collect async RPC
+        # futures into a list
+        out_futures = []
+        for x in iter(xs.split(self.split_size, dim=0)):
+            input_rref = RRef(x)
+            p1_out_rref = self.p1_rref.remote().forward(input_rref)
+            p2_out_rref = self.p2_rref.remote().forward(p1_out_rref)
+            p3_out_rref = self.p3_rref.remote().forward(p2_out_rref)
+            out_fut = self.p4_rref.rpc_async().forward(p3_out_rref)
+            out_futures.append(out_fut)
+
+        # collect and cat all output tensors into one tensor.
+        return torch.cat(torch.futures.wait_all(out_futures))
+
+    def parameter_rrefs(self):
+        remote_params = []
+        remote_params.extend(self.p1_rref.remote().parameter_rrefs().to_here())
+        remote_params.extend(self.p2_rref.remote().parameter_rrefs().to_here())
+        remote_params.extend(self.p3_rref.remote().parameter_rrefs().to_here())
+        remote_params.extend(self.p4_rref.remote().parameter_rrefs().to_here())
+        return remote_params
+
+
+#########################################################
+#                   Run RPC Processes                   #
+#########################################################
+
+num_batches = 1
+batch_size = 128
+image_w = 128
+image_h = 128
+
+
+def run_master(split_size):
+
+    # put the two model parts on workers.
+    model = DistAlexNet(split_size, ["worker1", "worker2", "worker3", "worker4"])
+    loss_fn = nn.MSELoss()
+    opt = DistributedOptimizer(
+        optim.SGD,
+        model.parameter_rrefs(),
+        lr=0.05,
+    )
+
+    one_hot_indices = torch.LongTensor(batch_size) \
+                           .random_(0, num_classes) \
+                           .view(batch_size, 1)
+
+    for i in range(num_batches):
+        print(f"Processing batch {i}")
+        # generate random inputs and labels
+        inputs = torch.randn(batch_size, 3, image_w, image_h)
+        labels = torch.zeros(batch_size, num_classes) \
+                      .scatter_(1, one_hot_indices, 1)
+
+        # The distributed autograd context is the dedicated scope for the
+        # distributed backward pass to store gradients, which can later be
+        # retrieved using the context_id by the distributed optimizer.
+        with dist_autograd.context() as context_id:
+            outputs = model(inputs)
+            dist_autograd.backward(context_id, [loss_fn(outputs, labels)])
+            opt.step(context_id)
+
+
+def run_worker(rank, world_size, split_size):
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '29500'
+    options = rpc.TensorPipeRpcBackendOptions(num_worker_threads=256, rpc_timeout=600)
+
+    import psutil
+    p = psutil.Process()
+    
+    if rank == 0:
+        p.cpu_affinity([0])
+        print(f"Child #{rank}: Set my affinity to {rank}, affinity now {p.cpu_affinity()}", flush=True)
+
+        rpc.init_rpc(
+            "master",
+            rank=rank,
+            world_size=world_size,
+            rpc_backend_options=options
+        )
+        run_master(split_size)
+    else:
+        p.cpu_affinity([rank-1])
+        print(f"Child #{rank}: Set my affinity to {rank}, affinity now {p.cpu_affinity()}", flush=True)
+
+        rpc.init_rpc(
+            f"worker{rank}",
+            rank=rank,
+            world_size=world_size,
+            rpc_backend_options=options
+        )
+        pass
+
+    # block until all rpcs finish
+    rpc.shutdown()
+
+
+if __name__=="__main__":
+    world_size = 5
+    tik = time.time()
+    mp.spawn(run_worker, args=(world_size, 16), nprocs=world_size, join=True)
+    tok = time.time()
+    print(f"execution time = {tok - tik}")
